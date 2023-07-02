1cc1e6a46075d70722999459034220280ea86439
dgkim2008 <dongg9318@gmail.com>
2023-07-02 14:51:16 +0900

    pet 화면 개발 시작, 추가로 더 작성하기

--- app/pet/index.js
+++ app/pet/index.js
@@ -1,12 +1,83 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button ,TextInput} from "react-native";
 import {Link} from 'expo-router'
 
 export default function Page() {
     return (
         <View style={styles.main}>
+            <View style={styles.button}>
+                <View style={styles.center}>
+                    <Text>반려동물:</Text>
+                </View>
+                <Button title="고양이" style={styles.buttoncolor}></Button>
+                <Button title="강아지"></Button>
+            </View>
+            <View style={styles.box}>
+                <TextInput style={styles.input}></TextInput>
+            </View>
+            <View style={styles.box}>
+                
+            </View>
+            <View style={styles.box}>
+                
+            </View>
+            <View style={styles.box}>
+            
+            </View>
+            <View style={styles.box}>
+                
+            </View>
+            <View style={styles.box}>
+                
+            </View>
+            <View style={styles.box}>
+            
+            </View>
+            <View style={styles.boxlast}>
+                
+            </View>
         </View>
     );
 }
 const styles = StyleSheet.create({
-
+    main: {
+        flex: 1,
+        marginLeft: 40,
+        marginBottom: 50
+    },
+    button: {
+        width: 300, 
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        textAlign: 'center',
+        paddingBottom: 15,
+        paddingTop: 15,
+        borderBottomWidth: 1,
+    },
+    center: {
+        display:'flex',
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+    box: {
+        height: 50,
+        display:'flex',
+        justifyContent: 'center',
+        textAlign: 'center',
+        width: 300,
+        borderBottomWidth: 1,
+        paddingBottom: 15,
+        flex: 1
+    },
+    boxlast: {
+        height: 50,
+        display:'flex',
+        justifyContent: 'center',
+        textAlign: 'center',
+        width: 300,
+        paddingBottom: 15,
+        flex: 1
+    }, 
+    input: {
+        placeholder: '반려동물의 이름을 입력해주세요'
+    }
 })
