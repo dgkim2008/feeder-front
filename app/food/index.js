54d780718ba750be06c2d5d9e65c00862cd89366
dgkim2008 <dongg9318@gmail.com>
2023-07-06 23:08:39 +0900

    정리

--- app/food/index.js
+++ app/food/index.js
@@ -1,20 +1,32 @@
 import { StyleSheet, Text, View, Switch} from "react-native";
+import React, {useState} from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import {Link} from 'expo-router'
 
 export default function Page() {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
     return (
         <View style={styles.main}>
             <View>
                 <AntDesign name="pluscircleo" size={20} color="black">
-                    <Text style={styles.plus}> 추가</Text>
+                    <Link style={styles.plus} href="/timeset"> 추가</Link>
                 </AntDesign>
             </View>
             <View>
                 <View>
-                    <Text>시간 설정</Text>
-                    <Text>요일/양</Text>
-                    <Switch></Switch>
+                    <View>
+                        <Text>시간 설정</Text>
+                        <Text>요일/양</Text>
+                    </View>
+                    <Switch
+                    trackColor={{false: '#767577', true: '#81c147'}}
+                    thumbColor={'white'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                    ></Switch>
                 </View>
             </View>
         </View>
---
+++ app/timeset/index.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import {Link} from 'expo-router'
+
+export default function Page() {
+    return (
+        <View style={styles.main}>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+
+})
