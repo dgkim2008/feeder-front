b9ed2b4f3c4e133dc47d41bb8a80fbd323bdcd7e
dgkim2008 <dongg9318@gmail.com>
2023-07-08 15:33:05 +0900

    첫화면 수정

--- app/index.js
+++ app/index.js
@@ -1,29 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
-import { AntDesign } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
 import { Link } from 'expo-router'
 
 export default function Page() {
   return (
     <View style={{
       flex: 1,
-      flexDirection: 'column',
     }}>
       <View style={styles.container}>
         <Text style={styles.title}>멍냥</Text>
-        <Text style={styles.title}>먹창고</Text>
+        <Link style={styles.title} href="/find">먹창고</Link>
       </View>
-      <View style={styles.container}>
-        <View style={styles.titlecontainer}>
-          <AntDesign name="plus" size={30} color="black">
-            <Link style={styles.find} href="/find">자동급식기찾기</Link>
-          </AntDesign>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <Ionicons name="home" size={100} color="black" />
-      </View>
-
     </View>
   );
 }
@@ -39,30 +25,11 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     padding: 24,
     backgroundColor: "white",
+    marginBottom: 100
   },
   title: {
     fontWeight: '700',
     fontSize: 55,
     color: 'black'
-  },
-  titlecontainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    backgroundColor: "white",
-    width: 260,
-    height: 150,
-    backgroundColor: 'white',
-    borderWidth: 1,
-    borderRadius: 20,
-    padding: 0,
-
-  },
-  find: {
-    fontWeight: '700',
-    fontSize: 25,
-    paddingBottom: 100,
-    paddingTop: 100
-  },
+  }
 });
--- app/pet/index.js
+++ app/pet/index.js
@@ -1,56 +1,65 @@
-import { StyleSheet, Text, View, Button ,TextInput} from "react-native";                                    
+import { StyleSheet, Text, View, Button ,TextInput, ScrollView} from "react-native";                                    
+import React, {useState} from 'react';
 
 export default function Page() {
+    const [info, setInfo] = useState({
+        type:"cat",
+        name:"",
+        date:"",
+        gen:"M",
+        weight:"",
+        neutered:true,
+        feed:"",
+        obesity:-1,
+    });
+    const ACTIVE = "red";
+    const DEACTIVE = "blue";
     return (
-        <View style={styles.main}>
-            <View style={styles.button}>
-                <View style={styles.center}>
-                    <Text>반려동물:</Text>
+        <ScrollView>
+            <View style={styles.main}>
+                <View style={styles.button}>
+                    <View style={styles.center}>
+                        <Text>반려동물:</Text>
+                    </View>
+                    <Button title="고양이" onPress={() => setInfo({...info, type:"cat"})} color={info.type === "cat" ? ACTIVE : DEACTIVE}></Button>
+                    <Button title="강아지" onPress={() => setInfo({...info, type:"dog"})} color={info.type === "dog" ? ACTIVE : DEACTIVE}></Button>
                 </View>
-                <Button title="고양이" onPress={() => console.log(this)}></Button>
-                <Button title="강아지"></Button>
-            </View>
-            <View style={styles.box}>
-                <TextInput style={styles.input} placeholder="반려동물의 이름을 입력해주세요" placeholderTextColor="black"></TextInput>
-            </View>
-            <View style={styles.box}>
-                <TextInput style={styles.input} placeholder="생년월일 ex) 2023-01-01" placeholderTextColor="black"></TextInput>
-            </View>
-            <View style={styles.button}>
-                <View style={styles.center}>
-                        <Text>성별:</Text>
+                <View style={styles.button}>
+                    <TextInput style={styles.input} onChangeText={name => setInfo({...info, name})} placeholder="반려동물의 이름을 입력해주세요" placeholderTextColor="black"></TextInput>
                 </View>
-                <Button title="남"></Button>                
-                <Button title="여"></Button>
+                <View style={styles.button}>
+                    <TextInput style={styles.input} onChangeText={date => setInfo({...info, date})} placeholder="생년월일 ex) 2023-01-01" placeholderTextColor="black"></TextInput>
                 </View>
-            <View style={styles.box}>
-                <TextInput style={styles.input} placeholder="몸무게 ex) 2.3kg" placeholderTextColor="black"></TextInput>
-            </View>
-            <View style={styles.button}>
-                <View style={styles.center}>
-                        <Text>중성화:</Text>
+                <View style={styles.button}>
+                    <View style={styles.center}>
+                            <Text>성별:</Text>
+                    </View>
+                    <Button title="남아" onPress={() => setInfo({...info, gen:"M"})} color={info.gen === 'M' ? ACTIVE : DEACTIVE}></Button>                
+                    <Button title="여아" onPress={() => setInfo({...info, gen:"F"})} color={info.gen === 'F' ? ACTIVE : DEACTIVE}></Button>
+                    </View>
+                <View style={styles.box}>
+                    <TextInput style={styles.input} onChangeText={weight => setInfo({...info, weight})} placeholder="몸무게 ex) 2.3kg" placeholderTextColor="black"></TextInput>
                 </View>
-                <Button title="했어요" color= 'red'></Button>
-                <Button title="안했어요"></Button>
-            </View>
-            <View style={styles.box}>
-                <TextInput style={styles.input} placeholder="사료 KG당 칼로리 ex) 110kcal" placeholderTextColor="black"></TextInput>
-            </View>
-            <View style={styles.button}>
-                <View style={styles.center}>
-                        <Text>비만여부:</Text>
+                <View style={styles.button}>
+                    <View style={styles.center}>
+                            <Text>중성화:</Text>
+                    </View>
+                    <Button title="했어요" onPress={() => setInfo({...info, neutered:true})} color={info.neutered === true ? ACTIVE : DEACTIVE}></Button>
+                    <Button title="안했어요" onPress={() => setInfo({...info, neutered:false})} color={info.neutered === false ? ACTIVE : DEACTIVE}></Button>
                 </View>
-                <Button title="O"></Button>
-                <Button title="X"></Button>
-            </View>
-            <View style={styles.button}>
-                <View style={styles.center}>
-                        <Text>저체중여부:</Text>
+                <View style={styles.button}>
+                    <TextInput style={styles.input} onChangeText={feed => setInfo({...info, feed})} placeholder="사료 KG당 칼로리 ex) 110kcal" placeholderTextColor="black"></TextInput>
                 </View>
-                <Button title="O"></Button>
-                <Button title="X"></Button>
+                <View style={styles.button}>
+                    <View style={styles.center}>
+                            <Text style={styles.text}>체중 선택</Text>
+                    </View>
+                    <Button title="비만" onPress={() => setInfo({...info, obesity:-1})} color={info.obesity === -1 ? ACTIVE : DEACTIVE}></Button>
+                    <Button title="보통" onPress={() => setInfo({...info, obesity:0})} color={info.obesity === 0 ? ACTIVE : DEACTIVE}></Button>
+                    <Button title="저체중" onPress={() => setInfo({...info, obesity:1})} color={info.obesity === 1 ? ACTIVE : DEACTIVE}></Button>
+                </View>
             </View>
-        </View>
+        </ScrollView>
     );
 }
 const styles = StyleSheet.create({
@@ -66,22 +75,12 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         paddingBottom: 15,
         paddingTop: 15,
-        //borderBottomWidth: 1,
+        height: 70
     },
     center: {
         display:'flex',
         justifyContent: 'center',
         textAlign: 'center'
-    },
-    box: {
-        height: 50,
-        display:'flex',
-        justifyContent: 'center',
-        textAlign: 'center',
-        width: 300,
-        //borderBottomWidth: 1,
-        paddingBottom: 15,
-        flex: 1
     },
     boxlast: {
         height: 50,
