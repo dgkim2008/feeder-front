b8a687540d26b00dc3ea5b4c4ebaff7dd5bd34ed
dgkim2008 <dongg9318@gmail.com>
2023-07-08 14:04:05 +0900

    food 변경

--- app/food/index.js
+++ app/food/index.js
@@ -5,7 +5,9 @@ import {Link} from 'expo-router'
 
 export default function Page() {
     const [isEnabled, setIsEnabled] = useState(false);
+    const [isEnabled2, setIsEnabled2] = useState(false);
     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const toggleSwitch2 = () => setIsEnabled2(previousState => !previousState);
 
     return (
         <View style={styles.main}>
@@ -14,19 +16,34 @@ export default function Page() {
                     <Link style={styles.plus} href="/timeset"> 추가</Link>
                 </AntDesign>
             </View>
-            <View>
+            <View style={styles.container}>
                 <View>
-                    <View>
-                        <Text>시간 설정</Text>
-                        <Text>요일/양</Text>
-                    </View>
+                    <Text style={styles.time}>시간 설정</Text>
+                    <Text>요일/양</Text>
+                </View>
+                <View>
                     <Switch
                     trackColor={{false: '#767577', true: '#81c147'}}
                     thumbColor={'white'}
                     ios_backgroundColor="#3e3e3e"
                     onValueChange={toggleSwitch}
                     value={isEnabled}
-                    ></Switch>
+                    />
+                </View>
+            </View>
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.time}>12: 00</Text>
+                    <Text>금요일/ 100g</Text>
+                </View>
+                <View>
+                    <Switch
+                    trackColor={{false: '#767577', true: '#81c147'}}
+                    thumbColor={'white'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch2}
+                    value={isEnabled2}
+                    />
                 </View>
             </View>
         </View>
@@ -35,8 +52,20 @@ export default function Page() {
 const styles = StyleSheet.create({
     main: {
         marginLeft: 20,
+        marginRight: 20,
     },
     plus: {
         fontWeight: 'bold'
-    }
+    },
+    container: {
+        height: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 15,
+        marginBottom: 15
+    }, 
+    time: {
+        fontSize: 16
+    },
+
 })
